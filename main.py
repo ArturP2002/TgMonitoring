a9129430319dad2483fcbfd380fb665285f2f0cf
ArturP2002 <145135267+ArturP2002@users.noreply.github.com>
2024-04-20 02:08:00 +0300

    Файл запуска

---
+++ main.py
@@ -0,0 +1,50 @@
+from bot_init import bot  # Импорт telegram-бота
+from BotFunctions.parsing_messages import message_parsing
+from telethon.sync import TelegramClient  # Класс, позволяющий подключаться к клиенту мессенджера
+from telethon.tl.types import PeerChannel  # Метод, определяющий объекты типа канал/чат
+import re
+from secrets import api_id, api_hash, phone  # Импорт конфиденциальной информации пользователя
+from BotFunctions.parsing_chats import chats_parsing
+from BotFunctions.retrieve_csv_element import get_last_msg
+from chat_lists import key_words, offset_id
+from keyboards import start_kb
+from telebot.types import Message
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message: Message):
+    """
+    Функция приветствия telegram-бота
+    :param message: сообщение пользователя
+    """
+    bot.reply_to(message, text='<b>Добро пожаловать, {name}!</b>\n\nС моей помощью вы можете проанализировать сообщения'
+                               ' указанного вами telegram-канал и найти необходимые вам по ключевым словам',
+                 reply_markup=start_kb,
+                 parse_mode='html')
+    bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Мониторинг')
+def monitoring(message: Message):
+    """
+    Функция мониторинга telegram-канала(чата)
+    :param message: сообщение пользователя
+    """
+    all_words = re.findall(r'\w+', str(get_last_msg()))  # Функция получения последнего отправленного сообщения
+    for i_word in key_words:
+        for j_word in all_words:
+            if i_word.lower() == j_word.lower():
+                username = message.from_user.username  # Ник пользователя в telegram
+                bot.send_message(chat_id='ID-ЧАТА',  # Ваш ID
+                                 text=f'<b>Было обнаружено следующее сообщение:</b> {all_words}\n<b>Имя пользователя'
+                                      f'(автора) сообщения:</b> {username}',
+                                 parse_mode='html')  # Функция, которая пересылает сообщение
+                break
+
+
+if __name__ == '__main__':
+    client = TelegramClient(phone, api_id, api_hash)  # Создаем наш клиент
+    client.start()  # Запускаем клиент и входим в telegram-аккаунт
+    first_result = chats_parsing(client)  # Результат парсинга пользователей telegram-канала(чата)
+    message_parsing(client, first_result, offset_id)  # Результат парсинга сообщений telegram-канала(чата)
+    bot.polling()  # Запуск бота
