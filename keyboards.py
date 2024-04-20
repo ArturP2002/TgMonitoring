fb0215dbc05f98ef171ee906a18a91c33a039778
ArturP2002 <145135267+ArturP2002@users.noreply.github.com>
2024-04-21 02:04:07 +0300

    Кнопка начала работы

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,6 @@
 import telebot
 
 # Клавиатура для начала работы
-start_kb = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+start_kb = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 button = telebot.types.KeyboardButton(text='Мониторинг')
 start_kb.add(button)
